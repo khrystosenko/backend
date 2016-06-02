fca2a728139b416d7b1eed7efa3da71664d1456b
Vova Minof <vovaminiof@gmail.com>
2016-06-02 19:12:05 +0200

    Language change based on lang GET parameter.

--- roomit/settings.py
+++ roomit/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware'
+    'libs.middleware.SessionBasedLocaleMiddleware'
 ]
 
 ROOT_URLCONF = 'roomit.urls'
