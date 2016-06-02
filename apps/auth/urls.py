31eb8d8f4b9653dd4404d8171904365e34f26230
Vova Minof <vovaminiof@gmail.com>
2016-06-02 19:02:27 +0200

    Added twitch.

--- apps/auth/urls.py
+++ apps/auth/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 
-from apps.auth.views import FacebookLoginView
+from apps.auth.views import FacebookLoginView, TwitchLoginView
 
 
 urlpatterns = [
     url(r'^$', include('rest_auth.urls')),
 
     url(r'^facebook/$', FacebookLoginView.as_view(), name='fb_login'),
+    url(r'^twitch/$', TwitchLoginView.as_view(), name='twitch_login'),
 ]
--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,6 +1,16 @@
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
+from allauth.socialaccount.providers.twitch.views import TwitchOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
 
 
 class FacebookLoginView(SocialLoginView):
     adapter_class = FacebookOAuth2Adapter
+    client_class = OAuth2Client
+    callback_url = 'http://localhost:8000/auth/facebook/'
+
+
+class TwitchLoginView(SocialLoginView):
+    adapter_class = TwitchOAuth2Adapter
+    client_class = OAuth2Client
+    callback_url = 'http://localhost:8000/auth/twitch/'
