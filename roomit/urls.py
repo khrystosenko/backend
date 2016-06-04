728a47f3cdd74a6a78c7af978c785f78f4a162ef
Vova Minof <vovaminiof@gmail.com>
2016-06-04 19:46:26 +0200

    Added serving static files for test server.

--- roomit/urls.py
+++ roomit/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -22,3 +24,8 @@ urlpatterns = [
     url(r'^subscribe/', include('apps.subscribe.urls')),
     url(r'^features/', include('apps.features.urls'))
 ]
+
+if settings.TEST_SERVER:
+    urlpatterns += [
+        url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT, 'show_indexes':True})
+    ]
